eeb8316fb4ffe8c0afd721e93d2c50a31d9af190
pdeka <pdeka@thoughtworks.com>
2009-04-01 10:22:25 +1100

    Added session reset

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,13 @@
 class LoginController < ApplicationController
   def login
+    reset_session
+
+  end
+
+  def reset_session
+    request.reset_session
+    @_session = request.session
+    response.session = @_session
   end
 
 end
