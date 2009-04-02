ad4e4d09c703bf3672e97c5e8f8d5c00b1f70633
Josh Male <jmale.home@gmail.com>
2009-04-02 11:42:55 +1100

    Cleaned up active scaffold for user ratings

--- app/controllers/user_ratings_controller.rb
+++ app/controllers/user_ratings_controller.rb
@@ -1,3 +1,6 @@
 class UserRatingsController < ApplicationController
-  active_scaffold :user_rating
+  active_scaffold :user_rating do |config|
+    config.label = 'User Ratings'
+    config.columns = [ :user, :dimension, :rating, :situational_data, :channel, :created_at, :updated_at]
+  end
 end
