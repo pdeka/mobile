07b9914c74c1aff55a686314cba4c187bdfa00d6
Josh Male <jmale.home@gmail.com>
2009-04-03 18:51:41 +1100

    Consolidate summary view into sit data view. Removed all client-side redirects

--- app/controllers/situational_data_entry_controller.rb
+++ app/controllers/situational_data_entry_controller.rb
@@ -8,17 +8,13 @@ class SituationalDataEntryController < ApplicationController
     situational_data = SituationalData.new
     situational_data.location = params['location']
     situational_data.accompanying_people = params['accompanying_people']
+    situational_data.significant_occurences = params['significant_occurences']
 
     rating_session = session[:rating_session]
     rating_session.situational_data = situational_data
     rating_session.save!
 
-    if params['answer'] == 'yes'
-      redirect_to '/significant_occurences'
-    else
-      redirect_to '/summary'
-    end
-
+    @rating_session = session[:rating_session]
   end
 
 end
