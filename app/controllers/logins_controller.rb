92b73500ba6c064418aed1731895f7dedfd0557d
Josh Male <jmale.home@gmail.com>
2009-04-03 17:23:30 +1100

    removed client side redirect for login to experiment on Nokia

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -16,7 +16,12 @@ class LoginsController < ApplicationController
     if (@user)
       session[:user] = @user
       open_ratings_session @user
-      redirect_to homes_path
+#      redirect_to homes_path
+
+      initialise_homepage_info
+      respond_to do |format|
+        format.iphone { render :template => 'homes/index' }
+      end
     else
       flash[:error] = 'Login was not successful.'
       respond_to do |format|
@@ -43,5 +48,16 @@ class LoginsController < ApplicationController
     session[:rating_session] = rating_session
   end
 
+  def initialise_homepage_info
+    @user = session[:user]
+    @dimensions = @user.dimensions.dup
 
+    # filter out dimensions which have already been rated
+    rating_session = session[:rating_session]
+    rating_session.user_ratings.each do |rating|
+      @dimensions.delete rating.dimension
+    end
+
+
+  end
 end
